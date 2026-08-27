9559ca5d0f0e82d2761668cee654639f1ca8dd78
Minyeong Jeong <hey.minyeong@gmail.com>
2026-08-27 23:32:41 +0900

    feat(input): add base input component

---
+++ packages/ui/src/input/input.stylex.ts
@@ -0,0 +1,55 @@
+import {
+  colorVars,
+  motionVars,
+  opacityVars,
+  radiusVars,
+  sizeVars,
+  spacingVars,
+  typographyVars,
+} from "@cachette/tokens/tokens.stylex";
+import * as stylex from "@stylexjs/stylex";
+
+export const styles = stylex.create({
+  root: {
+    backgroundColor: colorVars.bgSurface,
+    borderColor: colorVars.strokeDefault,
+    borderRadius: radiusVars.md,
+    borderStyle: "solid",
+    borderWidth: sizeVars.stroke,
+    color: colorVars.fgPrimary,
+    fontSize: typographyVars.fontSizeSm,
+    height: sizeVars.controlMd,
+    lineHeight: typographyVars.lineHeightNormal,
+    minWidth: 0,
+    outline: "none",
+    paddingInline: spacingVars.space3,
+    transitionDuration: motionVars.durationFast,
+    transitionProperty: "border-color, box-shadow, opacity",
+    transitionTimingFunction: motionVars.easingStandard,
+    width: "100%",
+    "::placeholder": {
+      color: colorVars.fgTertiary,
+    },
+    ":hover": {
+      borderColor: colorVars.strokeStrong,
+    },
+    ":focus-visible": {
+      borderColor: colorVars.strokeFocus,
+      outlineColor: colorVars.interactionSelected,
+      outlineOffset: sizeVars.stroke,
+      outlineStyle: "solid",
+      outlineWidth: sizeVars.focusRing,
+    },
+    ":disabled": {
+      backgroundColor: colorVars.bgSubtle,
+      cursor: "not-allowed",
+      opacity: opacityVars.disabled,
+    },
+    ":user-invalid": {
+      borderColor: colorVars.strokeFeedbackError,
+    },
+  },
+  invalid: {
+    borderColor: colorVars.strokeFeedbackError,
+  },
+});
